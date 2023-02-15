9575411559f4338e8526935663cf64ed4bae5b8c
Bogdan Covrig <bogcov@gmail.com>
2023-02-15 22:49:55 +0100

    feature: Add /app redirect to /links

---
+++ app/routes/app/index.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "@remix-run/node";
+
+import type { LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return redirect("/app/links");
+};
