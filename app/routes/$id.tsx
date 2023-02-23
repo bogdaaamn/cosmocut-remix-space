bd65a5cfc2fd3682d999642e7a74a9999e6965eb
Bogdan Covrig <bogcov@gmail.com>
2023-02-23 18:25:18 +0100

    feature: Add route for link redirects with error page

---
+++ app/routes/$id.tsx
@@ -0,0 +1,38 @@
+import { json, redirect } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import type { LoaderArgs } from "@remix-run/node";
+
+import { getLinkData } from "~/models/link.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  if (!params.id) {
+    throw new Error("Missing link ID");
+  }
+
+  try {
+    const data = await getLinkData(params.id);
+    return redirect(data.url);
+  } catch (e) {
+    return json({ id: params.id });
+  }
+};
+
+export default function RedirectSlug() {
+  return (
+    <div className="bg-dots h-full w-full">
+      <div className="text-center flex flex-col justify-center h-full">
+        <p className="mt-2 text-3xl font-medium text-gray-900">
+          Short link not found
+        </p>
+        <p className="mt-1 text-xl text-gray-500">
+          Go back to{" "}
+          <Link to="https://deta.space" className="font-medium">
+            de.ta
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+}
